759337963cbaa7e3e649e6cfc95464452e76a623
Rahul chidambaram <49508732+rahulchidambaram@users.noreply.github.com>
2026-04-13 12:31:58 +0530

    Update Restaurant_Bill.py

--- Assignment-1/Restaurant_Bill.py
+++ Assignment-1/Restaurant_Bill.py
@@ -1,7 +1,10 @@
+# Calculate restaurant bill
 def calculate_restaurant_bill(meal_cost):
+    #tax and tip is 5 %
     tax_and_tip = 0.05
     service_charge = meal_cost * 0.1
     amount_after_service = service_charge + meal_cost
+    # adding amount after service tax + tax + tip amount
     tax_tip_amount = amount_after_service * tax_and_tip
     total = amount_after_service + tax_tip_amount + tax_tip_amount
 
